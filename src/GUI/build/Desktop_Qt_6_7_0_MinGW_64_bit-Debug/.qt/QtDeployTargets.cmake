fbbb2497a97727740bc21e190fe1942a10b813ff
Daniel Geier <xgeierd00@stud.fit.vutbr.cz>
2024-04-20 22:27:43 +0200

    Renamed folder GUI to gui

--- src/GUI/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Hugocalc_FILE C:/Users/Grif9/OneDrive/Dokumenty/Hugocalc/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/Hugocalc.exe)
-set(__QT_DEPLOY_TARGET_Hugocalc_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
