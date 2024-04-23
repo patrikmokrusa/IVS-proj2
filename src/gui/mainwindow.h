cbf8cf3a431c5946b18ac5c3a999a6bd90d01b2f
Daniel Geier <xgeierd00@stud.fit.vutbr.cz>
2024-04-23 15:10:33 +0200

    Added dependecy gui folder to CMakeList

--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -9,7 +9,7 @@
 /**
  * @file mainwindow.h
  * @author Adam Černoš
- * 
+ *
  *
  * @brief Definice UI funkcí, Vytvořeno pomocí QT editoru
  */
@@ -17,87 +17,86 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 #include <QDialog>
 #include <QLabel>
+#include <QMainWindow>
+#include <QWidget>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 protected:
-    void keyPressEvent(QKeyEvent *event);
+  void keyPressEvent(QKeyEvent *event);
 
 public:
-
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private slots:
-    void result();
+  void result();
 
-    void reset();
+  void reset();
 
-    void handleOperation(QString operand);
+  void handleOperation(QString operand);
 
-    void on_button_0_clicked();
+  void on_button_0_clicked();
 
-    void on_button_1_clicked();
+  void on_button_1_clicked();
 
-    void on_button_2_clicked();
+  void on_button_2_clicked();
 
-    void on_button_3_clicked();
+  void on_button_3_clicked();
 
-    void on_button_4_clicked();
+  void on_button_4_clicked();
 
-    void on_button_5_clicked();
+  void on_button_5_clicked();
 
-    void on_button_6_clicked();
+  void on_button_6_clicked();
 
-    void on_button_7_clicked();
+  void on_button_7_clicked();
 
-    void on_button_8_clicked();
+  void on_button_8_clicked();
 
-    void on_button_9_clicked();
+  void on_button_9_clicked();
 
-    void on_button_ac_clicked();
+  void on_button_ac_clicked();
 
-    void on_button_del_clicked();
+  void on_button_del_clicked();
 
-    void on_button_times_ten_clicked();
+  void on_button_times_ten_clicked();
 
-    void on_button_plus_clicked();
+  void on_button_plus_clicked();
 
-    void on_button_minus_clicked();
+  void on_button_minus_clicked();
 
-    void on_button_times_clicked();
+  void on_button_times_clicked();
 
-    void on_button_div_clicked();
+  void on_button_div_clicked();
 
-    void on_button_equals_clicked();
+  void on_button_equals_clicked();
 
-    void on_button_negate_clicked();
+  void on_button_negate_clicked();
 
-    void on_button_decimal_clicked();
+  void on_button_decimal_clicked();
 
-    void on_button_y_exp_x_clicked();
+  void on_button_y_exp_x_clicked();
 
-    void on_button_factorial_clicked();
+  void on_button_factorial_clicked();
 
-    void on_pushButton_clicked();
+  void on_pushButton_clicked();
 
-    void on_exitButton_clicked();
+  void on_exitButton_clicked();
 
-    void on_button_1_over_x_clicked();
+  void on_button_1_over_x_clicked();
 
-    void on_button_exp_x_root_y_clicked();
+  void on_button_exp_x_root_y_clicked();
 
 private:
-    Ui::MainWindow *ui;
-    QDialog *dialog;
+  Ui::MainWindow *ui;
+  QDialog *dialog;
 };
 #endif // MAINWINDOW_H
