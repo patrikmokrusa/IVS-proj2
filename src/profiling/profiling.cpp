3824220c7795a0a738ddfd97cdf2af6cb52945a0
Daniel Geier <xgeierd00@stud.fit.vutbr.cz>
2024-04-24 00:47:30 +0200

    Added deps and targets in CMakeList

--- src/profiling/profiling.cpp
+++ src/profiling/profiling.cpp
@@ -10,61 +10,70 @@
  * @file profiling.cpp
  * @author Lukáš Jelínek
  *
- * @brief Výpočes směrodatné odchylky, který slouží k profilingu matematické knihovny
+ * @brief Výpočes směrodatné odchylky, který slouží k profilingu matematické
+ * knihovny
  */
-#include <iostream>
-#include <vector>
 #include "../mathlib.cpp"
-
-
+#include <iostream>
+#include <sstream>
+#include <vector>
 
 int main() {
-    int numbers;
-    std::vector<double> v;
+  int numbers;
+  std::vector<double> v;
 
-    for(int i = 0; i < 1000; i++)
-    {
-        scanf("%i", &numbers);
-        v.push_back(numbers);
+  std::string str;
+  std::getline(std::cin, str);
+  std::istringstream sstr(str);
+  int n;
+
+  while (sstr >> n) {
+    // scanf("%i", &numbers);
+    v.push_back(n);
+  }
+
+  /*
+    for (int i = 0; i < 1000; i++) {
+      scanf("%i", &numbers);
+      v.push_back(numbers);
     }
+    */
 
-    //Vypsani pole
-    //for(int i = 0; i < v.size(); i++){
-    //    printf("%lf\n", v[i]);
-    //}
+  // Vypsani pole
+  // for(int i = 0; i < v.size(); i++){
+  //     printf("%lf\n", v[i]);
+  // }
 
-    double temp = 0;
-    for(int i = 0; i < v.size(); i++){
-        temp = k_plus(temp, v[i]);
-    }
+  double temp = 0;
+  for (int i = 0; i < v.size(); i++) {
+    temp = k_plus(temp, v[i]);
+  }
 
-    temp = k_deleno(temp, v.size());
+  temp = k_deleno(temp, v.size());
 
+  double temp1;
+  for (int i = 0; i < v.size(); i++) {
+    temp1 = k_minus(v[i], temp);
+    temp1 = k_mocnina(temp1, 2);
+    v[i] = temp1;
+  }
 
-    double temp1;
-    for(int i = 0; i < v.size(); i++){
-        temp1 = k_minus(v[i], temp);
-        temp1 = k_mocnina(temp1, 2);
-        v[i] = temp1;
-    }
+  // printf("\n");
 
-    //printf("\n");
+  double temp2;
+  for (int i = 0; i < v.size(); i++) {
+    temp2 = k_plus(temp2, v[i]);
+  }
 
-    double temp2;
-    for(int i = 0; i < v.size(); i++){
-        temp2 = k_plus(temp2, v[i]);
+  double temp3;
 
-    }
-    
-    double temp3;
+  temp3 = k_minus(v.size(), 1);
+  temp2 = k_deleno(temp2, temp3);
+  temp2 = k_odmocnina(temp2, 2);
 
-    temp3 = k_minus(v.size(), 1);
-    temp2 = k_deleno(temp2, temp3);
-    temp2 = k_odmocnina(temp2, 2);
+  // printf("Výsledek je %lf", temp2);
+  printf("%lf", temp2);
+  printf("\n");
 
-    printf("Výsledek je %lf", temp2);
-    printf("\n");
-
-
-    return 0;
+  return 0;
 }
